1a1a4bc62f201c79eae65614f44c7f6e19409fcb
Jose Albizures <albizures3601@gmail.com>
2017-04-16 17:06:11 -0600

    fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-openssl aes-256-cbc -K $encrypted_8a195021a80c_key -iv $encrypted_8a195021a80c_iv -in deploy-key2.enc -out deploy-key2 -d
-rm deploy-key.enc
+openssl aes-256-cbc -K $encrypted_8a195021a80c_key -iv $encrypted_8a195021a80c_iv -in deploy-key2.enc -out deploy-key -d
+rm deploy-key2.enc
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
