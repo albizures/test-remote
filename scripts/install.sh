8f86016aad0a204b9d75952d55193313b6c52ff8
Jose Albizures <albizures3601@gmail.com>
2017-04-16 17:02:41 -0600

    new key

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-set -x # Show the output of the following commands (useful for debugging)
-    
-# Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_8a195021a80c_key -iv $encrypted_8a195021a80c_iv -in deploy-key.enc -out deploy-key -d
-rm deploy-key.enc # Don't need it anymore
+set -x
+
+openssl aes-256-cbc -K $encrypted_8a195021a80c_key -iv $encrypted_8a195021a80c_iv -in deploy-key2.enc -out deploy-key2 -d
+rm deploy-key.enc
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
